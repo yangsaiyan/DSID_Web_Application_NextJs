cc096d9a9348df6bd74a6688a80bc6318cd7c9b1
unknown <yangz030715@gmail.com>
2025-02-06 22:51:17 +0800

    fix: modified to correct import path

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
