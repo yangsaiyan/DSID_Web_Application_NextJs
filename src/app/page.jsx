34bfa17751c9ec4824d5d0e7842981adbc64d4b6
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-02-11 01:17:11 +0800

    test: fixing build error 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function page() {
 
     return (
       <Grid2 style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-        //<StyledLottie animationData={require("../../public/assets/lotties/StudentIdAnimation.json")} />
+        {/* <StyledLottie animationData={require("../../public/assets/lotties/StudentIdAnimation.json")} /> */}
         <Typography variant="h4" sx={{ fontFamily: "cursive", textAlign: "center" }}>Secure, Smart, and Decentralized – Your Student ID, Reimagined.</Typography>
       </Grid2>
     );
