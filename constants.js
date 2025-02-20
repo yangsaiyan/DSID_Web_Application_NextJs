90953373b08d885e60465f8cd2f9b917f78c3cbd
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-02-20 17:29:31 +0800

    feat: add dyna ic form input

---
+++ constants.js
@@ -0,0 +1,15 @@
+export const formPath = {
+  register: ["name", "studentId", "icNumber", "email", "phoneNumber", "permanentHomeAddress", "walletAddress", "token"],
+  pushEmail: ["email", "studentId"],
+}
+
+export const userData = {
+  name: "Name",
+  studentId: "Student ID",
+  icNumber: "IC Number",
+  email: "Email",
+  phoneNumber: "Phone Number",
+  permanentHomeAddress: "Permanent Home Address",
+  walletAddress: "Wallet Address",
+  token: "Token",
+};
--- src/app/components/form/form.jsx
+++ src/app/components/form/form.jsx
@@ -8,30 +8,68 @@ import {
   StyledTextField,
   TextFieldContainer,
 } from "./styles";
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "../../../../redux/actions/user_action";
+import { usePathname, useRouter } from "next/navigation";
+import { formPath, userData } from "../../../../constants";
+import { Router } from "next/router";
 
 export default function form() {
-
   const dispatch = useDispatch();
+  const pathname = usePathname();
 
   const user = useSelector((state) => state.user);
 
   const [formData, setFormData] = useState({
     name: "",
     studentId: "",
+    icNumber: "",
+    email: "",
+    phoneNumber: "",
+    permanentHomeAddress: "",
     walletAddress: "",
     token: "",
   });
+  const [formDisplay, setFormDisplay] = useState([]); // set to empty when submitted
+  const [formInput, setFormInput] = useState({});
 
-  console.log(user);
+  useEffect(() => {
+    if (pathname?.includes("register")) {
+      setFormDisplay(formPath?.register);
+    } else if (pathname?.includes("pushEmail")) {
+      setFormDisplay(formPath?.pushEmail);
+    }
+  }, [Router.isReady]);
+
+  useEffect(() => {
+    filterFormInput(formDisplay);
+  }, [formDisplay]);
+
+  const filterFormInput = (formDisplay) => {
+    for (const [key, value] of Object.entries(userData)) {
+      formDisplay?.forEach((item) => {
+        if (key === item) {
+          setFormInput((prev) => ({
+            ...prev,
+            [key]: value,
+          }));
+        }
+      });
+    }
+  };
 
   return (
     <StyledBox>
-      <TextFieldContainer>
-        <StyledTextField label="Student ID" />
-        <Grid2
+      {Object?.entries(formInput)?.map(([key, value]) => {
+        return (
+          <TextFieldContainer>
+            <StyledTextField label={userData[key]} />
+          </TextFieldContainer>
+        );
+      })}
+      <StyledTextField label="Student ID" />
+      {/* <Grid2
           display={"flex"}
           flexDirection={"column"}
           justifyContent={"center"}
@@ -41,12 +79,11 @@ export default function form() {
         >
           <StyledTextField label="Wallet Address" />
           <ConnectWalletButton>xxxxxxxxxxxxxxxxxxxx</ConnectWalletButton>
-        </Grid2>
-      </TextFieldContainer>
-      <CTAButtonContainer>
+        </Grid2> */}
+      {/* <CTAButtonContainer>
         <CTAButton type={"reset"}>Reset</CTAButton>
         <CTAButton type={"submit"} onClick={()=>{dispatch(getUser({userName: "LLLL", userAge: "123"}))}}>Submit</CTAButton>
-      </CTAButtonContainer>
+      </CTAButtonContainer> */}
     </StyledBox>
   );
 }
