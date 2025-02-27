17e2137ccbf844ffd0ff5e54ab46cdb158daf5f2
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-02-28 00:14:06 +0800

    fix: checking components one by one to find out which component caused build error

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "../../wallet_connect/config";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
-import Navbar from "@/components/navbar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 
 const queryClient = new QueryClient();
 
