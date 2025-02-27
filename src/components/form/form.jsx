08cfd7429f6336c4c996284fc629b424fecdc339
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-02-28 00:03:22 +0800

    fix: build error

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,147 +1,147 @@
-"use client";
-import { Grid2 } from "@mui/material";
-import {
-  ConnectWalletButton,
-  CTAButton,
-  CTAButtonContainer,
-  StyledBox,
-  StyledTextField,
-  TextFieldContainer,
-} from "./styles";
-import { useEffect, useRef, useState } from "react";
-import { useDispatch } from "react-redux";
-import { usePathname, useSearchParams } from "next/navigation";
-import { formPath, userData } from "../../../constants";
-import Loading from "../loading/loading";
-import emailjs from "@emailjs/browser";
-import { isEmpty } from "lodash";
+// "use client";
+// import { Grid2 } from "@mui/material";
+// import {
+//   ConnectWalletButton,
+//   CTAButton,
+//   CTAButtonContainer,
+//   StyledBox,
+//   StyledTextField,
+//   TextFieldContainer,
+// } from "./styles";
+// import { useEffect, useRef, useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { usePathname, useSearchParams } from "next/navigation";
+// import { formPath, userData } from "../../../constants";
+// import Loading from "../loading/loading";
+// import emailjs from "@emailjs/browser";
+// import { isEmpty } from "lodash";
 
-export default function form() {
-  const dispatch = useDispatch();
-  const pathname = usePathname();
-  const params = useSearchParams();
+// export default function form() {
+//   const dispatch = useDispatch();
+//   const pathname = usePathname();
+//   const params = useSearchParams();
 
-  const formRef = useRef(null);
+//   const formRef = useRef(null);
 
-  const [formData, setFormData] = useState({
-    name: "",
-    studentId: "",
-    icNumber: "",
-    email: "",
-    phoneNumber: "",
-    permanentHomeAddress: "",
-    walletAddress: "",
-    token: "",
-  });
-  const [formDisplay, setFormDisplay] = useState([]); // set to empty when submitted
-  const [formInput, setFormInput] = useState({});
-  const [loading, setLoading] = useState(true);
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     studentId: "",
+//     icNumber: "",
+//     email: "",
+//     phoneNumber: "",
+//     permanentHomeAddress: "",
+//     walletAddress: "",
+//     token: "",
+//   });
+//   const [formDisplay, setFormDisplay] = useState([]); // set to empty when submitted
+//   const [formInput, setFormInput] = useState({});
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (pathname?.includes("register")) {
-      setFormDisplay(formPath?.register);
-    } else if (pathname?.includes("push")) {
-      setFormDisplay(formPath?.pushEmail);
-    }
-  }, [pathname]);
+//   useEffect(() => {
+//     if (pathname?.includes("register")) {
+//       setFormDisplay(formPath?.register);
+//     } else if (pathname?.includes("push")) {
+//       setFormDisplay(formPath?.pushEmail);
+//     }
+//   }, [pathname]);
 
-  useEffect(() => {
-    console.log(params.get("studentId"));
-    setFormData({
-      ...formData,
-      studentId: params?.get("studentId"),
-    });
-  }, []);
+//   useEffect(() => {
+//     console.log(params.get("studentId"));
+//     setFormData({
+//       ...formData,
+//       studentId: params?.get("studentId"),
+//     });
+//   }, []);
 
-  useEffect(() => {
-    filterFormInput(formDisplay);
-    setLoading(false);
-  }, [formDisplay]);
+//   useEffect(() => {
+//     filterFormInput(formDisplay);
+//     setLoading(false);
+//   }, [formDisplay]);
 
-  const filterFormInput = (formDisplay) => {
-    for (const [key, value] of Object.entries(userData)) {
-      formDisplay?.forEach((item) => {
-        if (key === item) {
-          setFormInput((prev) => ({
-            ...prev,
-            [key]: value,
-          }));
-        }
-      });
-    }
-  };
+//   const filterFormInput = (formDisplay) => {
+//     for (const [key, value] of Object.entries(userData)) {
+//       formDisplay?.forEach((item) => {
+//         if (key === item) {
+//           setFormInput((prev) => ({
+//             ...prev,
+//             [key]: value,
+//           }));
+//         }
+//       });
+//     }
+//   };
 
-  const submitForm = (e) => {
-    e.preventDefault();
+//   const submitForm = (e) => {
+//     e.preventDefault();
 
-    if (pathname?.includes("push") && formRef) {
-      emailjs
-        .sendForm(
-          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-          formRef?.current,
-          {
-            publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
-          }
-        )
-        .then(
-          () => {
-            console.log("SUCCESS!"); //add toast later
-          },
-          (error) => {
-            console.log("FAILED...", error.text); //add toast later
-          }
-        );
-    } else if (pathname?.includes("register")) {
-    }
-  };
+//     if (pathname?.includes("push") && formRef) {
+//       emailjs
+//         .sendForm(
+//           process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+//           process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+//           formRef?.current,
+//           {
+//             publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+//           }
+//         )
+//         .then(
+//           () => {
+//             console.log("SUCCESS!"); //add toast later
+//           },
+//           (error) => {
+//             console.log("FAILED...", error.text); //add toast later
+//           }
+//         );
+//     } else if (pathname?.includes("register")) {
+//     }
+//   };
 
-  const formInputErrorValidation = () => {};
+//   const formInputErrorValidation = () => {};
 
-  return loading ? (
-    <Loading />
-  ) : (
-    <StyledBox
-      ref={formRef}
-      component="form"
-      onSubmit={submitForm}
-      path={pathname}
-    >
-      <TextFieldContainer
-        sx={{ paddingTop: pathname?.includes("register") && "96px" }}
-      >
-        {Object?.entries(formInput)?.map(([key, value]) => {
-          return (
-            <>
-              {key !== "walletAddress" && key !== "token" && (
-                <StyledTextField
-                  label={value}
-                  name={key}
-                  defaultValue={!isEmpty(formData[key]) ? formData[key] : ""}
-                  disabled={!isEmpty(formData[key])}
-                />
-              )}
-              {key === "walletAddress" && (
-                <Grid2
-                  display={"flex"}
-                  flexDirection={"column"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                  gap={"5px"}
-                  width={"100%"}
-                >
-                  <StyledTextField label={value} />
-                  <ConnectWalletButton>Connect Wallet</ConnectWalletButton>
-                </Grid2>
-              )}
-            </>
-          );
-        })}
-      </TextFieldContainer>
-      <CTAButtonContainer>
-        <CTAButton type={"reset"}>Reset</CTAButton>
-        <CTAButton type={"submit"}>Submit</CTAButton>
-      </CTAButtonContainer>
-    </StyledBox>
-  );
-}
+//   return loading ? (
+//     <Loading />
+//   ) : (
+//     <StyledBox
+//       ref={formRef}
+//       component="form"
+//       onSubmit={submitForm}
+//       path={pathname}
+//     >
+//       <TextFieldContainer
+//         sx={{ paddingTop: pathname?.includes("register") && "96px" }}
+//       >
+//         {Object?.entries(formInput)?.map(([key, value]) => {
+//           return (
+//             <>
+//               {key !== "walletAddress" && key !== "token" && (
+//                 <StyledTextField
+//                   label={value}
+//                   name={key}
+//                   defaultValue={!isEmpty(formData[key]) ? formData[key] : ""}
+//                   disabled={!isEmpty(formData[key])}
+//                 />
+//               )}
+//               {key === "walletAddress" && (
+//                 <Grid2
+//                   display={"flex"}
+//                   flexDirection={"column"}
+//                   justifyContent={"center"}
+//                   alignItems={"center"}
+//                   gap={"5px"}
+//                   width={"100%"}
+//                 >
+//                   <StyledTextField label={value} />
+//                   <ConnectWalletButton>Connect Wallet</ConnectWalletButton>
+//                 </Grid2>
+//               )}
+//             </>
+//           );
+//         })}
+//       </TextFieldContainer>
+//       <CTAButtonContainer>
+//         <CTAButton type={"reset"}>Reset</CTAButton>
+//         <CTAButton type={"submit"}>Submit</CTAButton>
+//       </CTAButtonContainer>
+//     </StyledBox>
+//   );
+// }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,60 +1,60 @@
-"use client";
-import { Grid2 } from "@mui/material";
-import {
-  ConnectWalletButton,
-  NavbarContainer,
-  NavbarContent,
-  NavbarContentContainer,
-  StyledBox,
-  StyledText,
-  TextContainer,
-} from "./styles";
-import { useAccount } from "wagmi";
-import { Account } from "./Account/account";
-import { WalletButton } from "./WalletButton/WalletButton";
+// "use client";
+// import { Grid2 } from "@mui/material";
+// import {
+//   ConnectWalletButton,
+//   NavbarContainer,
+//   NavbarContent,
+//   NavbarContentContainer,
+//   StyledBox,
+//   StyledText,
+//   TextContainer,
+// } from "./styles";
+// import { useAccount } from "wagmi";
+// import { Account } from "./Account/account";
+// import { WalletButton } from "./WalletButton/WalletButton";
 
-export default function Navbar(props) {
-  const { ref } = props;
-  // const { address } = useAccount()
-  // const { disconnect } = useDisconnect()
+// export default function Navbar(props) {
+//   const { ref } = props;
+//   // const { address } = useAccount()
+//   // const { disconnect } = useDisconnect()
 
-  function ConnectWallet() {
-    const { isConnected } = useAccount();
-    if (isConnected) return <Account />;
-    return <WalletButton />;
-  }
+//   function ConnectWallet() {
+//     const { isConnected } = useAccount();
+//     if (isConnected) return <Account />;
+//     return <WalletButton />;
+//   }
 
-  return (
-    <Grid2 ref={ref} sx={{ padding: "2.5% 5% 0 5%", width: "100%" }}>
-      <NavbarContainer>
-        <NavbarContentContainer>
-          <Grid2
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-            backgroundColor={"#d9d9d9"}
-            borderRadius={"20% 0 0 20%"}
-            zIndex={1000}
-          >
-            <StyledBox />
-          </Grid2>
-          <NavbarContent>
-            <TextContainer href={"/"}>
-              <StyledText>Home</StyledText>
-            </TextContainer>
-            <TextContainer href={"/search"}>
-              <StyledText>Search</StyledText>
-            </TextContainer>
-            <TextContainer href={"/register"}>
-              <StyledText>Register</StyledText>
-            </TextContainer>
-            <TextContainer href={"/push"}>
-              <StyledText>Push Email</StyledText>
-            </TextContainer>
-          </NavbarContent>
-        </NavbarContentContainer>
-        <ConnectWallet></ConnectWallet>
-      </NavbarContainer>
-    </Grid2>
-  );
-}
+//   return (
+//     <Grid2 ref={ref} sx={{ padding: "2.5% 5% 0 5%", width: "100%" }}>
+//       <NavbarContainer>
+//         <NavbarContentContainer>
+//           <Grid2
+//             display={"flex"}
+//             alignItems={"center"}
+//             justifyContent={"center"}
+//             backgroundColor={"#d9d9d9"}
+//             borderRadius={"20% 0 0 20%"}
+//             zIndex={1000}
+//           >
+//             <StyledBox />
+//           </Grid2>
+//           <NavbarContent>
+//             <TextContainer href={"/"}>
+//               <StyledText>Home</StyledText>
+//             </TextContainer>
+//             <TextContainer href={"/search"}>
+//               <StyledText>Search</StyledText>
+//             </TextContainer>
+//             <TextContainer href={"/register"}>
+//               <StyledText>Register</StyledText>
+//             </TextContainer>
+//             <TextContainer href={"/push"}>
+//               <StyledText>Push Email</StyledText>
+//             </TextContainer>
+//           </NavbarContent>
+//         </NavbarContentContainer>
+//         <ConnectWallet></ConnectWallet>
+//       </NavbarContainer>
+//     </Grid2>
+//   );
+// }
