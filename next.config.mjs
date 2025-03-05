b1d7b775cc32bb94c4d5e3cf45ceffd6a6945b2d
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-03-06 01:17:06 +0800

    fix: build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "text-encoding": require.resolve("text-encoding"),
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
