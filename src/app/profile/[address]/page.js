c63b539b47116c09cb6c969ea8138c1cbc4908db
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-03-18 21:07:47 +0800

    test: handle callback

--- src/app/profile/[address]/page.js
+++ src/app/profile/[address]/page.js
@@ -1,26 +1,27 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { getStudent } from "hooks/GunDB";
-import { useRouter } from "next/router";
 
 export default function Profile() {
+  const searchParams = useSearchParams();
+  const address = searchParams.get("address"); 
+  const callback = searchParams.get("callback"); 
+  
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const { address, callback } = router.query;
 
   useEffect(() => {
-    if (!router.isReady || !address || !callback) return; 
+    if (!address || !callback) return;
 
     async function fetchProfile() {
       try {
-        setLoading(true);
         const data = await getStudent(address);
         setProfile(data);
 
         if (data) {
           const redirectUrl = `${callback}?data=${encodeURIComponent(
-            JSON.stringify(data) 
+            JSON.stringify(data)
           )}`;
           window.location.href = redirectUrl;
         }
@@ -32,7 +33,7 @@ export default function Profile() {
     }
 
     fetchProfile();
-  }, [router.isReady, address, callback]);
+  }, [address, callback]);
 
   return <div>{loading ? "Loading..." : "Redirecting..."}</div>;
 }
