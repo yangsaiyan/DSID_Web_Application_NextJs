12a8548059bbe21d80dca99f2f4cbd4f1dfb1e8f
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-04-04 12:30:43 +0800

    fix: fallback if background image not loaded

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,12 +2,6 @@
 import { useEffect, useState } from "react";
 import "./globals.css";
 import { Providers } from "./Providers";
-import dynamic from "next/dynamic";
-
-const Loading = dynamic(() => import("../components/loading/loading"), {
-  ssr: false,
-});
-
 export default function RootLayout({ children }) {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -22,21 +16,25 @@ export default function RootLayout({ children }) {
       lang="en"
       style={{ height: "100vh", display: "flex", flexDirection: "column" }}
     >
-      <body
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100%",
-          backgroundImage: `linear-gradient(145deg, rgba(73,73,73,0.6) 0%, rgba(0,0,0,1) 100%), 
+      {isLoading ? (
+        <body style={{backgroundColor: "black", height: "100vh", width: "100vw"}} />
+      ) : (
+        <body
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            backgroundImage: `linear-gradient(145deg, rgba(73,73,73,0.6) 0%, rgba(0,0,0,1) 100%), 
           url("/assets/images/indexBackground.jpg")`,
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        {isLoading ? <Loading /> : <Providers>{children}</Providers>}
-      </body>
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Providers>{children}</Providers>
+        </body>
+      )}
     </html>
   );
 }
